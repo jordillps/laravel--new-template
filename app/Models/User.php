63eaae7a76da7b9190b07f8667b0a45192098ac5
jordillps <jordillps@gmail.com>
2025-10-13 19:22:49 +0200

    Instalación Filament - Crear Usuario Admin

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Filament\Panel;
+use Filament\Models\Contracts\FilamentUser;
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -53,4 +56,11 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    //Poden acceder al panell complet de Filament  todos los usuarios  
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
+
 }
